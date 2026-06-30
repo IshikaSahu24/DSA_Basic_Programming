193cca32164c0e5c719a9eb44256e26d8e8d359a
IshikaSahu24 <ishikasahu24052005@gmail.com>
2026-06-30 16:04:46 +0530

    Program 3 & 4 are added

---
+++ Program3.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Program3 {
+
+    // WAP to check whether the number is even or odd
+
+    //Using arithmetic operator
+    public void evenorodd(int n){
+        if(n % 2 == 0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+    //Using ternary operator
+    public void EvenorOdd(int n){
+        String result = (n % 2 == 0)? "Even" : "Odd";
+        System.out.println(result);
+    }
+
+    //Using bitwise operator
+    public void even_or_odd(int n){
+        if((n & 1) == 0){
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the digit to check even or odd : ");
+        int n= sc.nextInt();
+
+        // Create an object of Program3 class
+        Program3 p = new Program3();
+        p.evenorodd(n);
+        p.EvenorOdd(n);
+        p.even_or_odd(n);
+    }
+}
---
+++ Program4.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Program4 {
+
+    // WAP to find factorial of a number
+
+    //using iterative method
+    public long factorial(long n){
+        if(n<0){
+            throw new IllegalArgumentException( "Factorial is not defined for negative numbers"); 
+        }
+
+        long result =1;
+
+        for(long i=2;i<=n;i++){
+            result=result*i;
+        }
+        return result;
+    }
+
+
+    public long factorialRecursive(int n){
+        if(n<0){
+            throw new IllegalArgumentException( "Factorial is not defined for negative numbers");
+        }
+        if(n==0){
+            return 1;
+        }
+        return factorialRecursive(n-1)*n;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the digit to find factorial : ");
+        int n= sc.nextInt();
+
+        Program4 p = new Program4();
+        System.out.println("Factorial of " + n + " (iterative): " + p.factorial(n));
+        System.out.println("Factorial of " + n + " (recursive): " + p.factorialRecursive(n));
+    }
+}
